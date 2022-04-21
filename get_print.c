4336f9b89edadbdc363dd58e2cea48cfc861fbdb
hefeholuwah <ilieas300@gmail.com>
2022-04-20 23:13:26 -0700

    handled missing initializer

--- get_print.c
+++ get_print.c
@@ -6,7 +6,7 @@
  * @s: character that holds the conversion specifier
  * Description: the function loops through the structs array
  * func_arr[] to find a match between the specifier passed to _printf
- * and the first element of the struct, and then the appropriate
+ * and the first element of the struct, and then the approriate
  * printing function
  * Return: a pointer to the matching printing function
  */
@@ -27,7 +27,7 @@ int (*get_print(char s))(va_list, flags_t *)
 		{'S', print_bigS},
 		{'p', print_address},
 		{'%', print_percent}
-	};
+		};
 	int flags = 14;
 
 	register int i;
